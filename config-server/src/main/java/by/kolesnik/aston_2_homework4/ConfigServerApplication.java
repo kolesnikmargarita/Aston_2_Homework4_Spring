5ff5cac6e0b9f0a4955133241d5aaaf2c1924e7a
mkole <margaritakolesnik2003@gmail.com>
2026-09-19 00:58:56 +0300

    add microservice patterns with using spring cloud

---
+++ config-server/src/main/java/by/kolesnik/aston_2_homework4/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package by.kolesnik.aston_2_homework4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
